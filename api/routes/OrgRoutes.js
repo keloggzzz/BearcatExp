08d08fb48a60027b2b005e86b6eda60847b295a5
keloggzzz <kelis.clark10@gmail.com>
2025-03-27 13:58:56 -0400

    removed comments from books

--- api/routes/OrgRoutes.js
+++ api/routes/OrgRoutes.js
@@ -44,33 +44,4 @@ orgRouter.get("/getOrg", async (req, res) => {
     }
   });
 
-
-
-// change from books to organizations....also, how to add a org when an org member is validated...
-
-//   orgRouter.get("/addpost", async (req, res) => {
-//     try {
-
-//         var title="Coding Journal";
-//         var author="Jimmy John";
-//         var price=90.98;
-//         var catid=5;
-//         var qry="Insert into books (title, author, price, category_id) VALUES ("
-//         + "'"+title+"',"
-//         + "'"+author+"',"
-//         +price+","
-//         +catid+")";
-
-//         console.log(qry);
-
-//       const result = await pool.query(qry);
-//       console.log(result);
-//       res.json({ans: 1});
-     
-//     } catch (error) {
-//       console.error("Query error:", error);
-//       res.status(500).json({ error: "Database query failed" });     
-//     }
-//   });
-
 export default orgRouter;
